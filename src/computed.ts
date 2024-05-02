cc25b5c28a76d4d925e90d665f6c18299f4e5a65
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-05-02 12:51:31 +0200

    Don't recompute when dependent signals come back to previous values used for last computation
    (cherry picked from commit 49e85fead6f6f010ce031c4dbf7308fd3b1f857f)

--- src/computed.ts
+++ src/computed.ts
@@ -16,6 +16,7 @@ import {
   REACTIVE_NODE,
   ReactiveNode,
   SIGNAL,
+  Version,
 } from './graph.js';
 
 /**
@@ -47,6 +48,7 @@ export interface ComputedNode<T> extends ReactiveNode {
   markedToRecompute: boolean;
 
   equal: ValueEqualityFn<T>;
+  equalCache: Record<number, boolean> | null;
 }
 
 export type ComputedGetter<T> = (() => T) & {
@@ -115,11 +117,35 @@ const COMPUTED_NODE = /* @__PURE__ */ (() => {
     dirty: true,
     error: null,
     equal: defaultEquals,
+    equalCache: null,
 
     producerMustRecompute(node: ComputedNode<unknown>): boolean {
       // Force a recomputation if there's no current value, or if the current value is in the
       // process of being calculated (which should throw an error).
       return node.value === UNSET || node.value === COMPUTING || node.markedToRecompute;
+    },
+
+    producerEquals(node: ComputedNode<unknown>, value: unknown, valueVersion: Version) {
+      if (
+        valueVersion + 1 === node.version || // equal is called before the version is incremented
+        value === ERRORED ||
+        node.value === ERRORED ||
+        value === COMPUTING ||
+        node.value === COMPUTING ||
+        value === UNSET ||
+        node.value === UNSET
+      ) {
+        return false;
+      }
+      let res = node.equalCache?.[valueVersion];
+      if (res == null) {
+        res = !!node.equal.call(node.wrapper, value, node.value);
+        if (!node.equalCache) {
+          node.equalCache = {};
+        }
+        node.equalCache[valueVersion] = res;
+      }
+      return res;
     },
 
     producerRecomputeValue(node: ComputedNode<unknown>): void {
